045b672bfe72b18988a846dc6c6a423e768733da
jipenaflor <rome.img23@gmail.com>
2024-09-30 22:21:38 +0800

    integrated datasource via autoconfiguration

--- myfancypdfinvoices-springboot/src/main/java/com/jeromepenaflor/myfancypdfinvoices/springboot/service/InvoiceService.java
+++ myfancypdfinvoices-springboot/src/main/java/com/jeromepenaflor/myfancypdfinvoices/springboot/service/InvoiceService.java
@@ -3,24 +3,32 @@ package com.jeromepenaflor.myfancypdfinvoices.springboot.service;
 import com.jeromepenaflor.myfancypdfinvoices.springboot.model.Invoice;
 import com.jeromepenaflor.myfancypdfinvoices.springboot.model.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class InvoiceService {
 
-    private List<Invoice> invoices = new CopyOnWriteArrayList<>();
-
     private final UserService userService;
     private final String cdnUrl;
+    private final JdbcTemplate jdbcTemplate;
 
-    public InvoiceService(UserService userService, @Value("${cdn.url}") String cdnUrl) {
+    public InvoiceService(UserService userService,
+                          @Value("${cdn.url}") String cdnUrl, JdbcTemplate jdbcTemplate) {
         this.userService = userService;
         this.cdnUrl = cdnUrl;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @PostConstruct
@@ -36,18 +44,46 @@ public class InvoiceService {
     }
 
     public List<Invoice> findAll() {
-        return invoices;
+        return jdbcTemplate.query("select id, user_id, pdf_url, amount from invoices", (resultSet, rowNum) -> {
+            Invoice invoice = new Invoice();
+            invoice.setId(resultSet.getObject("id").toString());    // Not a simple string, but a UUID string
+            invoice.setPdfUrl(resultSet.getString("pdf_url"));
+            invoice.setUserId(resultSet.getString("user_id"));
+            invoice.setAmount(resultSet.getInt("amount"));
+            return invoice;
+        });
     }
 
     public Invoice create(String userId, Integer amount) {
-        User user = userService.findById(userId);
-        if (user == null) {
-            throw new IllegalStateException();
-        }
+        String generatedPdfUrl = cdnUrl + "/images/default/sample.pdf";
 
-        // TODO real pdf creation and storing it on network server
-        Invoice invoice = new Invoice(userId, amount, cdnUrl + "/images/default/sample.pdf");
-        invoices.add(invoice);
+        // make sure that the generated primary key is available via the keyholder
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection
+                    .prepareStatement("insert into invoices (user_id, pdf_url, amount) values (?, ?, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
+            // setting parameters replaced the (?, ?, ?); safeguard against SQL-injections
+            ps.setString(1, userId);
+            ps.setString(2, generatedPdfUrl);
+            ps.setInt(3, amount);
+            return ps;
+        }, keyHolder);
+
+        /* return the auto-generated UUID primary key from the database
+        GeneratedKeyHolder has a convenient way to return a numeric one, but not for the UUIDs, hence,
+        the use of ternary operators
+        */
+        String uuid = !keyHolder.getKeys().isEmpty() ? ((UUID) keyHolder.getKeys().
+                values().iterator().next()).toString() : null;
+
+        // create invoice object
+        Invoice invoice = new Invoice();
+        invoice.setId(uuid);
+        invoice.setPdfUrl(generatedPdfUrl);
+        invoice.setAmount(amount);
+        invoice.setUserId(userId);
         return invoice;
     }
 }
